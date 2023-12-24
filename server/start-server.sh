224fcab163469994e3ce4803946c717b0f6141f0
root <root@vmi404032.contaboserver.net>
2023-12-24 01:29:26 +0100

    View fixed

--- server/start-server.sh
+++ server/start-server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 
-
 # read config file if exist
 [ -f /etc/shell_sock/config/*.conf ] &&
      (  
@@ -11,6 +10,7 @@
          set +a
          echo "Configurations are loaded"
      )
+
 
 
 usage()
@@ -30,66 +30,70 @@ error()
 help()
 {
     echo
-	echo "To share accept terminal PTY requires certificates wich is signed by any CA"
-	echo "Generate private key: root@shell_sock:~\# openssl genrsa -out certs/server.key 4096"
-	echo "Generate CSR key:     root@shell_sock:~\# openssl req -new -key certs/server.key -out certs/server.csr"
-	echo "Send CSR file to CA and obtain signed PEM or CRT file and store certs folder (x509)"
-	echo "Get CA public key chain"
-	echo ""
-    echo "  {-k|--key     }  private key   -- Set prvate key     or   root@shell_sock:~# export KEY="
-    echo "  {-c|--cert    }  public key    -- Set public key     or   root@shell_sock:~# export CERT="
-    echo "  {-C|--ca-cert }  CA file       -- Set CA public key  or   root@shell_sock:~# export CA_CERT="
-    echo "  {-p|--port    }  PORT          -- Set listen port    or   root@shell_sock:~# export PORT="
-	echo ""               
+    echo "To share terminal to server requires certificates wich is signed by any CA"
+    echo "Generate private key: root@shell_sock:~\# openssl genrsa -out certs/server.key 4096"
+    echo "Generate CSR key:     root@shell_sock:~\# openssl req -new -key certs/server.key -out certs/server.csr"
+    echo "Send CSR file to CA and obtain signed PEM or CRT file and store certs folder (x509)"
+    echo "Get CA public key chain"
+    echo ""
+    echo "  {-k|--key        }  private key  -- Set prvate key       or   root@shell_sock:~# export KEY="
+    echo "  {-c|--cert       }  public key   -- Set public key       or   root@shell_sock:~# export CERT="
+    echo "  {-C|--ca-cert    }  CA file      -- Set CA public key    or   root@shell_sock:~# export CA_CERT="
+    echo "  {-p|--port       }  PORT         -- Set listen port      or   root@shell_sock:~# export PORT="
+    echo "  {-s|--server     }  SERVER       -- Set server ip|domain or   root@shell_sock:~# export SERVER="
+    echo "  {-l|--local-port }  SERVER       -- Set server ip|domain or   root@shell_sock:~# export LPORT="
+    echo ""               
     exit 0
 }
 
 # input arguments
 no_args="0"
 while test $# -gt 0
-    do
-        case "$1" in
-        (-k|--key)
-            shift
-            [ $# = 0 ] && error "No private key specified"
-            export KEY="$1";
-            shift;;
-        (-c|--cert)
-            shift
-            [ $# = 0 ] && error "No public key specified"
-            export CERT="$1"
-            shift;;
-        (-C|--ca-cert)
-            shift
-            [ $# = 0 ] && error "No ca public key specified"
-            export CA_CERT="$1"
-            shift;;
-        (-p|--port)
-            shift
-            [ $# = 0 ] && error "No tcp listen port specified"
-            export PORT="$1"
-            shift;;
-        (-h|--help)
-            help;;
-        (*) help;;
-        esac
-		
-    done
+     do
+          case "$1" in
+              (-k|--key)
+                  shift
+                  [ $# = 0 ] && error "No private key specified"
+                  export KEY="$1";
+                  shift;;
+             (-c|--cert)
+                  shift
+                  [ $# = 0 ] && error "No public key specified"
+                  export CERT="$1"
+                  shift;;
+              (-C|--ca-cert)
+                  shift
+                  [ $# = 0 ] && error "No ca public key specified"
+                  export CA_CERT="$1"
+                  shift;;
+              (-p|--port)
+                  shift
+                  [ $# = 0 ] && error "No tcp server port specified"
+                  export PORT="$1"
+                  shift;;
+              (-s|--server)
+                  shift
+                  [ $# = 0 ] && error "No tcp server specified"
+                  export SERVER="$1"
+                  shift;;
+              (-l|--local-port)
+                  shift
+                  [ $# = 0 ] && error "No reverse local port"
+                  export LPORT="$1"
+                  shift;;
+              (-h|--help)
+                  help;;
+              (*) help;;
+          esac
 
-$([ ! -z $KEY ] && [ ! -z $CERT ] && [ ! -z $CA_CERT ] && [ ! -z $PORT ]) && no_args=1 || no_args=0
+done
+
+$([ ! -z $KEY ] && [ ! -z $CERT ] && [ ! -z $CA_CERT ] && [ ! -z $PORT ] && [ ! -z $SERVER ] && [ ! -z $LPORT ]) && no_args=1 || no_args=0
 [[ "$no_args" -ne "1" ]] && { echo "";echo "ERROR: Some of arguments are not specified !! "; help; exit 1; }
 
-echo "SERVER START 0.0.0.0:$PORT"
-socat openssl-listen:$PORT,fork,cert=$CERT,key=$KEY,cafile=$CA_CERT,verify=4 'system:
-{
-    # read inial message as port
-	read port
-	# Check if input is a number between 1024 and 65535
-	if echo "$port" | grep -Eq "^[1-9][0-9]{3,4}$" ; then
-	  echo "Illegal port"
-	else
-	  # proxy TCP traffic to localhost:$port
-	  socat - TCP4:127.0.0.1:$port 
-	fi
-}'  >/dev/null 2>&1
 
+echo "..Startging to connect to: $SERVER:$PORT to accept connection 127.0.0.1:$LPORT"
+while true ; do 
+  socat OPENSSL:$SERVER:$PORT,cafile=$CA_CERT,key=$KEY,cert=$CERT,verify=4 system:'echo $LPORT; /bin/bash',pty,stderr,setsid,echo=0; 
+  sleep 5s
+done
